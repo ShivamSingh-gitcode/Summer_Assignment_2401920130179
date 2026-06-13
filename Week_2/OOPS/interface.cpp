fb6aa228cba2538f31500b7b7ff632687c62025e
ShivamSingh-gitcode <singhshivam6417@gmail.com>
2026-06-13 19:46:32 +0530

    WEEK 2 DAY OOPS SOLUTION

---
+++ Week_2/OOPS/interface.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+
+class Playable {
+public:
+    virtual void play() = 0; 
+};
+
+
+class Veena : public Playable {
+public:
+    void play() override {
+        cout << "Playing Veena" << endl;
+    }
+};
+
+
+class Saxophone : public Playable {
+public:
+    void play() override {
+        cout << "Playing Saxophone" << endl;
+    }
+};
+
+int main() {
+    
+    Veena veena;
+    veena.play();
+
+   
+    Saxophone saxophone;
+    saxophone.play();
+
+   
+    Playable* p;
+
+    p = &veena;
+    p->play();
+
+    p = &saxophone;
+    p->play();
+
+    return 0;
+}
